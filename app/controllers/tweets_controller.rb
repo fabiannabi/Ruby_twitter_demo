7d46b7f0c0cb742d0d1e753c44084f6f1933653b
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-04-23 16:44:23 -0500

    initial

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,33 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create content: params["content"]
+    redirect_to action: :index
+  end
+
+  def edit
+    @tweet = Tweet.find(params["id"])
+
+  end
+
+  def update
+    tweet = Tweet.find(params["id"])
+
+    tweet.update content: (params["content"])
+    redirect_to action: :index
+  end
+
+  def destroy
+    tweet =  Tweet.find(params["id"])
+    tweet.destroy
+
+    redirect_to action: :index
+  end
+end
